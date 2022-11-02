5d5c6d044dd676206c0f3369d5a556658af2953a
Sergey <mulichev@gmail.com>
2022-11-02 11:36:55 +0300

    added datastore constants

--- app/src/main/java/com/example/snakegame/util/Constants.kt
+++ app/src/main/java/com/example/snakegame/util/Constants.kt
@@ -2,5 +2,10 @@ package com.example.snakegame.util
 
 object Constants {
 
+    const val PREFERENCES_NAME = "snake_game_preferences"
+    const val PREFERENCES_HIGH_SCORE_1_KEY = "high_score_1"
+    const val PREFERENCES_HIGH_SCORE_2_KEY = "high_score_2"
+    const val PREFERENCES_HIGH_SCORE_3_KEY = "high_score_3"
+
     const val BOARD_SIZE = 16
 }
