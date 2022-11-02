9543a803774e7c03726992da2a9e196e0ff340fa
Sergey <mulichev@gmail.com>
2022-11-02 18:03:02 +0300

    added navigation to screens

--- app/src/main/java/com/example/snakegame/navigation/NavGraph.kt
+++ app/src/main/java/com/example/snakegame/navigation/NavGraph.kt
@@ -5,21 +5,23 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.snakegame.presentation.screen.game.GameScreen
+import com.example.snakegame.presentation.screen.high_scores.HighScoresScreen
+import com.example.snakegame.presentation.screen.menu.MenuScreen
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Game.route
+        startDestination = Screen.Menu.route
     ) {
         composable(route = Screen.Menu.route) {
-
+            MenuScreen(navController = navController)
         }
         composable(route = Screen.Game.route) {
-            GameScreen()
+            GameScreen(navController = navController)
         }
         composable(route = Screen.HighScores.route) {
-
+            HighScoresScreen(navController = navController)
         }
 
     }
