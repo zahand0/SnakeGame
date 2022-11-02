24134d00e4d780ee3046c35b27d3e7f54e4eadee
Sergey <mulichev@gmail.com>
2022-11-02 18:04:55 +0300

    theming game screen

--- app/src/main/java/com/example/snakegame/presentation/screen/game/GameScreen.kt
+++ app/src/main/java/com/example/snakegame/presentation/screen/game/GameScreen.kt
@@ -3,91 +3,107 @@ package com.example.snakegame.presentation.screen.game
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.example.snakegame.R
 import com.example.snakegame.data.model.Coordinate
 import com.example.snakegame.domain.game.GameStatus
 import com.example.snakegame.domain.game.SnakeDirections
-import kotlinx.coroutines.flow.forEach
+import com.example.snakegame.presentation.common.TopBar
 
 @Composable
 fun GameScreen(
+    navController: NavHostController,
     gameViewModel: GameViewModel = hiltViewModel()
 ) {
     val gameState = gameViewModel.gameStateFlow.collectAsState(initial = null)
     val score = gameViewModel.score
     val highScores = gameViewModel.getHighScores().collectAsState(initial = listOf())
-    when (gameState.value?.gameStatus) {
-        GameStatus.PLAY -> {
-            Column(
-//            modifier = Modifier.padding(contentPadding),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Score: ${score.value}")
-                gameState.value?.let {
-                    Board(it)
-                }
-                GameController(
-                    onDirectionChange = {
-                        when (it) {
-                            SnakeDirections.LEFT -> {
-                                gameViewModel.makeMove(Coordinate(x = -1, y = 0))
-                            }
-                            SnakeDirections.RIGHT -> {
-                                gameViewModel.makeMove(Coordinate(x = 1, y = 0))
-                            }
-                            SnakeDirections.UP -> {
-                                gameViewModel.makeMove(Coordinate(x = 0, y = -1))
-                            }
-                            SnakeDirections.DOWN -> {
-                                gameViewModel.makeMove(Coordinate(x = 0, y = 1))
+    Scaffold(
+        topBar = {
+            TopBar(title = stringResource(id = R.string.your_score).format(score.value)) {
+                navController.popBackStack()
+            }
+        },
+        backgroundColor = MaterialTheme.colors.background
+    ) { paddingValues ->
+        when (gameState.value?.gameStatus) {
+            GameStatus.PLAY -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    gameState.value?.let {
+                        Board(it)
+                    }
+                    GameController(
+                        onDirectionChange = {
+                            when (it) {
+                                SnakeDirections.LEFT -> {
+                                    gameViewModel.makeMove(Coordinate(x = -1, y = 0))
+                                }
+                                SnakeDirections.RIGHT -> {
+                                    gameViewModel.makeMove(Coordinate(x = 1, y = 0))
+                                }
+                                SnakeDirections.UP -> {
+                                    gameViewModel.makeMove(Coordinate(x = 0, y = -1))
+                                }
+                                SnakeDirections.DOWN -> {
+                                    gameViewModel.makeMove(Coordinate(x = 0, y = 1))
+                                }
                             }
                         }
-                    }
-                )
+                    )
 
+                }
             }
-        }
-        GameStatus.LOSE -> {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "YOU LOSE")
-                Text(text = "HIGH SCORES")
-                Column {
-                    highScores.value.forEach {
-                        Text(text = it.toString())
+            GameStatus.LOSE -> {
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "YOU LOSE")
+                    Text(text = "HIGH SCORES")
+                    Column {
+                        highScores.value.forEach {
+                            Text(text = it.toString())
+                        }
+                    }
+                    Button(onClick = { gameViewModel.restartGame() }) {
+                        Text(text = "RESTART")
                     }
                 }
-                Button(onClick = { gameViewModel.restartGame() }) {
-                    Text(text = "RESTART")
-                }
             }
-        }
-        else -> {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Button(onClick = { gameViewModel.startGame() }) {
-                    Text(text = "START")
+            else -> {
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = { gameViewModel.startGame() }) {
+                        Text(text = "START")
+                    }
                 }
             }
         }
     }
-
 
 }
