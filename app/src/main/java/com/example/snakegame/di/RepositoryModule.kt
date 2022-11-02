cf5837cb54229ba0f49bcc74f8236c2b58d6f3a0
Sergey <mulichev@gmail.com>
2022-11-02 11:38:05 +0300

    providing DataStoreOperations

---
+++ app/src/main/java/com/example/snakegame/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package com.example.snakegame.di
+
+import android.content.Context
+import com.example.snakegame.data.repository.DataStoreOperationsImpl
+import com.example.snakegame.domain.repository.DataStoreOperations
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStoreOperations(
+        @ApplicationContext context: Context
+    ): DataStoreOperations {
+        return DataStoreOperationsImpl(context = context)
+    }
+}
